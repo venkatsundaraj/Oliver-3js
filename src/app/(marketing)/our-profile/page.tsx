faeceeb56e02142c2dfa7e4515e59e4aa53058d2
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-10-28 18:45:54 +0530

    arrow-icon

--- src/app/(marketing)/our-profile/page.tsx
+++ src/app/(marketing)/our-profile/page.tsx
@@ -109,7 +109,7 @@ const page: FC<pageProps> = ({}) => {
                     className={cn(
                       "inline-flex items-center justify-center px-6 py-2 bg-secondary-foreground text-foreground font-paragraph text-extra_paragraph_heading min-w-[300px] md:min-w-[400px] relative",
                       i % 2 === 0
-                        ? `before:hidden before:md:block before:md:content-[''] before:w-2/3 before:h-[1px] before:absolute before:top-[50%] before:translate-y-[-50%] before:left-[100%] before:bg-foreground`
+                        ? `before:hidden before:md:block before:md:content-[''] before:w-0 before:md:w-2/3 before:h-[1px] before:absolute before:top-[50%] before:translate-y-[-50%] before:left-[100%] before:bg-foreground`
                         : ""
                     )}
                   >
@@ -123,7 +123,13 @@ const page: FC<pageProps> = ({}) => {
                 <li
                   className={cn(
                     "w-0 h-0 absolute border-r-[20px] border-l-[20px] border-l-transparent border-r-transparent border-t-[35px] border-t-foreground left-[50%] translate-x-[-50%] hidden md:block",
-                    `top-[${(i + 1) * 20}px]`
+                    {
+                      "top-[20%]": i === 0,
+                      "top-[40%]": i === 1,
+                      "top-[58%]": i === 2,
+                      "top-[76%]": i === 3,
+                      "top-[100%]": i === 4,
+                    }
                   )}
                 />
               ))}
