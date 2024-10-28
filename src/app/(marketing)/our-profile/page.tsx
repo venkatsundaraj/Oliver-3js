f9c37a21848123960cf303fa47028b4794dffb58
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-10-28 16:49:39 +0530

    hello

--- src/app/(marketing)/our-profile/page.tsx
+++ src/app/(marketing)/our-profile/page.tsx
@@ -98,7 +98,7 @@ const page: FC<pageProps> = ({}) => {
           </div>
         </div>
         <div className="container">
-          <div className="min-w-full max-w-6xl p-8 md:p-12 border border-foreground">
+          <div className="min-w-full max-w-6xl p-8 md:p-12 border border-foreground relative">
             <ul className="grid grid-cols-1 md:grid-cols-2 gap-12 relative ">
               {valueEngineContent.map((item, i) => (
                 <li
@@ -109,7 +109,7 @@ const page: FC<pageProps> = ({}) => {
                     className={cn(
                       "inline-flex items-center justify-center px-6 py-2 bg-secondary-foreground text-foreground font-paragraph text-extra_paragraph_heading min-w-[300px] md:min-w-[400px] relative",
                       i % 2 === 0
-                        ? `before:md:content-[''] before:w-2/3 before:h-[1px] before:absolute before:top-[50%] before:translate-y-[-50%] before:left-[100%] before:bg-foreground`
+                        ? `before:hidden before:md:block before:md:content-[''] before:w-2/3 before:h-[1px] before:absolute before:top-[50%] before:translate-y-[-50%] before:left-[100%] before:bg-foreground`
                         : ""
                     )}
                   >
@@ -118,9 +118,14 @@ const page: FC<pageProps> = ({}) => {
                 </li>
               ))}
             </ul>
-            <ul className="relative">
+            <ul className="absolute w-full h-full bg-transparent top-0 left-0">
               {Array.from({ length: 5 }).map((_, i) => (
-                <li>{}</li>
+                <li
+                  className={cn(
+                    "w-0 h-0 absolute border-r-[20px] border-l-[20px] border-l-transparent border-r-transparent border-t-[35px] border-t-foreground left-[50%] translate-x-[-50%] hidden md:block",
+                    `top-[${(i + 1) * 20}px]`
+                  )}
+                />
               ))}
             </ul>
           </div>
